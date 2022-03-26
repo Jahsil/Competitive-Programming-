a5833b40b2111f08cc817c9d93687a379253721a
Eyouel Haile <63557233+Jahsil@users.noreply.github.com>
2022-03-26 23:25:17 +0300

    Update 153.FindMinimuminRotatedSortedArray.py

--- BinarySearch/153.FindMinimuminRotatedSortedArray.py
+++ BinarySearch/153.FindMinimuminRotatedSortedArray.py
@@ -10,3 +10,22 @@ class Solution:
             else:
                 end = mid
         return nums[start]
+    
+    
+    # An alternative solution
+    
+    class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        start = 0
+        end = len(nums) - 1
+    
+        while start <= end:
+            mid = start + (end - start)//2
+        
+            if(nums[mid - 1] > nums[mid]): 
+                return nums[mid]
+            elif(nums[mid] > nums[end]): 
+                start = mid + 1 
+            else: 
+                end = mid - 1
+        return nums[mid]
