cf6e4ef774f57d6b003581effc42e0757f9316c4
Eyouel Haile <63557233+Jahsil@users.noreply.github.com>
2022-03-27 16:49:59 +0300

    Create 81.SearchinRotatedSortedArrayII.py

---
+++ BinarySearch/81.SearchinRotatedSortedArrayII.py
@@ -0,0 +1,42 @@
+start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] == nums[end]:
+                end -= 1
+            elif nums[mid] < nums[end]:
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            else:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+        return False
+      
+      
+      # Alternative solution
+      
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] == nums[end]:
+                end -= 1
+            elif nums[mid] < nums[end]:
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            else:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+        return False
