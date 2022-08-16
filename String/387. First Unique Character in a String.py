cc9d667dd72487580b053cf50ecb146a12068b81
Eyouel Haile <63557233+Jahsil@users.noreply.github.com>
2022-08-16 14:26:42 +0300

    Create 387. First Unique Character in a String.py

---
+++ String/387. First Unique Character in a String.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = {}
+        for i in range(len(s)):
+            freq[s[i]] = 0        
+        for i in range(len(s)):
+            if s[i] in freq.keys():
+                freq[s[i]] += 1
+        for i in range(len(s)):
+            if freq[s[i]] == 1:
+                return i
+            
+        return -1
+           
