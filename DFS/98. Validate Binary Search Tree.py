fb737ecc6446ed5b500b809df072ac7926e4b612
Eyouel Haile <63557233+Jahsil@users.noreply.github.com>
2022-08-17 19:53:09 +0300

    Update 98. Validate Binary Search Tree.py

--- DFS/98. Validate Binary Search Tree.py
+++ DFS/98. Validate Binary Search Tree.py
@@ -16,5 +16,34 @@ class Solution:
         if not (root.val > l and root.val < r):
             return False
         return self.dfs(root.left , l , root.val) and self.dfs(root.right , root.val , r)
+    
+    
+    
+    
+    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        result = []
+        def dfs(root):
+            if not root:
+                return 
+            nonlocal result
+            dfs(root.left)
+            result.append(root.val)
+            dfs(root.right)
+            
+        dfs(root)
+        for i in range(1 , len(result)):
+            if result[i-1] >= result[i]:
+                return False
+        return True
+        
             
         
