0fafd85f0ab71a044bf9953df906f6c6f55fa97b
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-08-24 19:22:37 +0300

    Update 231. Power of Two.py

--- Math/231. Power of Two.py
+++ Math/231. Power of Two.py
@@ -6,4 +6,15 @@ class Solution:
             n/= 2
             
         return n == 1
+    
+    
+# recursive solution
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True 
+        if n < 1 :
+            return False
+        return self.isPowerOfTwo(n/2)
         
