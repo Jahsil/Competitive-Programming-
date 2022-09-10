57fc7e14ea866eb92da0c655f9ddd28c5c7b1eb1
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-09-10 22:20:02 +0300

    Update and rename target sum.py to 494. Target Sum.py

--- Dynamic Programming/494. Target Sum.py
+++ Dynamic Programming/494. Target Sum.py
@@ -1,15 +1,15 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}
+        dp = dict()
         def recursion(index , targetSum):
             if index == len(nums):
                 return 1 if target == targetSum else 0
             if (index , targetSum) in dp:
                 return dp[(index , targetSum)]
+            
             dp[(index , targetSum)] = recursion(index + 1 , targetSum + nums[index])+recursion(index + 1 , targetSum - nums[index])
             
             return dp[(index , targetSum)]
         
+      
         return recursion(0 , 0)
-
-
