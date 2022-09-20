c656cb385cfe55d55a7893361d5d31b2261d8e89
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-09-21 01:03:20 +0300

    Create 2413. Smallest Even Multiple.py

---
+++ Math/2413. Smallest Even Multiple.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        i = 2
+        
+        while i % 2 != 0 or i % n != 0:
+            i += 1
+            
+        return i
