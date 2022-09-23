fcafbd67114cd3cd911416765f01e8e9b9c531d8
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-09-23 17:51:49 +0300

    Update 1680. Concatenation of Consecutive Binary Numbers.py

--- Daily Questions ( Sept 2022)/1680. Concatenation of Consecutive Binary Numbers.py
+++ Daily Questions ( Sept 2022)/1680. Concatenation of Consecutive Binary Numbers.py
@@ -12,4 +12,20 @@ class Solution:
         for i in range(len(arr)-1 , -1 , -1):
             newString += arr[i][2:]
         return int(newString , 2) % MOD
+    
+    
+# Faster solution
+
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+        curr = 0
+        for i in range(n + 1):
+            m = bin(i)[2:]
+            
+            curr <<= len(m)
+            curr |= i
+            curr %= MOD
+            
+        return curr
             
