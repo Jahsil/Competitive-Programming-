9992c2091880d731962bf22e4da42a0ff425385e
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-09-23 18:03:47 +0300

    Create 541. Reverse String II.py

---
+++ String/541. Reverse String II.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        ls = list(s)
+        for i in range(0, len(ls), 2*k):
+            ls[i:i+k] = reversed(ls[i:i+k])
+        return "".join(ls)
