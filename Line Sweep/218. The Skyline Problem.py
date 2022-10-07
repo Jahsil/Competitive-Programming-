f2c92c5e0e2c2cdabee815dd808c7428e74368e2
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-10-07 16:07:35 +0300

    Create 218. The Skyline Problem.py

---
+++ Line Sweep/218. The Skyline Problem.py
@@ -0,0 +1,29 @@
+from sortedcontainers import SortedList
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        d = defaultdict(list)
+        
+        for left , right , height in buildings:
+            d[left].append((-1 , height))
+            d[right].append((1 , height))
+
+        sl = SortedList()
+        sl.add(0)
+       
+        
+        result = []
+        last_height = 0
+        
+        for x in sorted(d.keys()):
+            for t , h in d[x]:
+                if t == -1:
+                    sl.add(h)
+                else:
+                    sl.remove(h)
+            current_height = sl[-1]
+            if current_height != last_height:
+                result.append((x , current_height))
+            last_height = current_height
+                
+                
+        return result
