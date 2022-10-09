110349691be029452ec9115eae6881ea416551f5
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-10-09 03:28:05 +0300

    Update 653. Two Sum IV - Input is a BST.py

--- Daily Questions (Oct 2022)/653. Two Sum IV - Input is a BST.py
+++ Daily Questions (Oct 2022)/653. Two Sum IV - Input is a BST.py
@@ -29,4 +29,26 @@ class Solution:
                 start += 1
         return False
             
-            
+# Using a hash set but slower solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        s = set()
+        
+        def dfs(root):
+            if not root:
+                return False
+            diff = k - root.val
+            if diff in s:
+                return True
+            else:
+                s.add(root.val)
+                
+            return dfs(root.left) or dfs(root.right)
+        return dfs(root)
