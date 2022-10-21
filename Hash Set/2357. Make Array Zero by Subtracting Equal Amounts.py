3fa16518ad1766d8a17a15fa1dd8bbebf3d626af
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-10-21 19:34:49 +0300

    Update 2357. Make Array Zero by Subtracting Equal Amounts.py

--- Hash Set/2357. Make Array Zero by Subtracting Equal Amounts.py
+++ Hash Set/2357. Make Array Zero by Subtracting Equal Amounts.py
@@ -15,3 +15,11 @@ class Solution:
                 
         return minOperations
                     
+        
+# Using a hash set 
+
+class Solution:
+   def minimumOperations(self, nums: List[int]) -> int:
+       return len(set(x for x in nums if x > 0))
+                    
+        
