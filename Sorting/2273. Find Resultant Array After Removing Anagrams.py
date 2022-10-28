847189352cfe36dc4e428e9428b2497fd695e6ad
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-10-28 19:26:29 +0300

    Update 2273. Find Resultant Array After Removing Anagrams.py

--- Sorting/2273. Find Resultant Array After Removing Anagrams.py
+++ Sorting/2273. Find Resultant Array After Removing Anagrams.py
@@ -9,3 +9,22 @@ class Solution:
 
             i += 1
         return words
+    
+# Constant space
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        
+        def getCount(word):
+            count = [0] * 26 
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            return count
+        
+        
+        i = 0
+        while i < len(words) - 1:
+            if getCount(words[i]) == getCount(words[i + 1]):
+                words.remove(words[i + 1])
+                continue
+            i += 1
+        return words
