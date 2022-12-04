816d60878f8aeec33b07a41f685ddb17d0e02f59
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-12-04 15:48:38 +0300

    Create 724. Find Pivot Index.py

---
+++ Prefix Sum/724. Find Pivot Index.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        N = len(nums)
+        prefix = [0] * (N + 1)
+        suffix = [0] * (N + 1)
+        
+        for i in range(N):
+            prefix[i+1] = prefix[i] + nums[i]
+            
+        for i in range(N-1 ,-1 , -1):
+            suffix[i] = suffix[i + 1] + nums[i]
+
+        for i in range(1 , N+1):
+            if (prefix[i-1] == suffix[i]):
+                return i-1
+        return -1
+            
+        
+        
+        
