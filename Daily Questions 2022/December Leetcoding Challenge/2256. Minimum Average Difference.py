22516868f4e59d4f75b16dd006ea3d787ce568ae
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-12-04 13:20:06 +0300

    Update 2256. Minimum Average Difference.py

--- Daily Questions 2022/December Leetcoding Challenge/2256. Minimum Average Difference.py
+++ Daily Questions 2022/December Leetcoding Challenge/2256. Minimum Average Difference.py
@@ -19,6 +19,49 @@ class Solution:
         minValue = min(ans)
         return ans.index(minValue)
             
+        
+        
+        
+# much faster solution
+        
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        N = len(nums)
+        
+        prefix = [0] * (N + 1)
+        suffix = [0] * (N + 1)
+        
+        for i in range(N):
+            prefix[i+1] = prefix[i] + nums[i]
+        for i in range(N - 1 , - 1 , -1):
+            suffix[i] = suffix[i+1] + nums[i]
+ 
+        minIndex = 0
+        minValue = float('inf')
+        
+        for i in range(N):
+            
+            left = prefix[i + 1]
+            left //= i + 1
+            
+            right = suffix[i + 1]
+            if (N - i - 1) != 0:
+                right //= N - i - 1
+                
+            diff = abs(left - right)
+            
+            if diff < minValue:
+                minValue = diff
+                minIndex = i
+                
+        return minIndex
+        
+        
+        
+        
+        
+        
+        
             
             
             
