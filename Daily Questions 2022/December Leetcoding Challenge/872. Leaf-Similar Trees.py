02c98216771ac3ba314b4b1dc144460c05742f58
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2022-12-08 17:56:27 +0300

    Update 872. Leaf-Similar Trees.py

--- Daily Questions 2022/December Leetcoding Challenge/872. Leaf-Similar Trees.py
+++ Daily Questions 2022/December Leetcoding Challenge/872. Leaf-Similar Trees.py
@@ -22,4 +22,28 @@ class Solution:
         b = arr
         return a == b
 
+#Using different approach
         
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def leafs(root):
+            arr = []
+
+            def dfs(root):
+                if not root:
+                    return 
+
+                if not root.left and not root.right:
+                    arr.append(root.val)
+                dfs(root.left)
+                dfs(root.right)
+
+            dfs(root)
+            return arr
+        return leafs(root1) == leafs(root2)
