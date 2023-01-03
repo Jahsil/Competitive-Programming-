ce7fd7e1964dbb2b10309111bb5b290847d022a9
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2023-01-03 20:49:16 +0300

    Create 520. Detect Capital.py

---
+++ Daily Questions 2023/January LeetCoding Challenge/520. Detect Capital.py
@@ -0,0 +1,7 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        
+        if word == word.upper() or word == word.lower() or (word[0] == word[0].upper() and word[1:] == word[1:].lower()):
+            return True
+        else:
+            return False
