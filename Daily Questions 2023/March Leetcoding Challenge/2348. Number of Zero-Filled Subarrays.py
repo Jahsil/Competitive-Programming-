ead964a2eb90404b8ffcfb3f2e4b62dc90f987d7
Eyouel Kibret <63557233+Jahsil@users.noreply.github.com>
2023-03-22 00:21:08 +0300

    Create 2348. Number of Zero-Filled Subarrays.py

---
+++ Daily Questions 2023/March Leetcoding Challenge/2348. Number of Zero-Filled Subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        total , current = 0 , 0 
+
+        for num in nums:
+            if num == 0 :
+                current += 1
+                total += current
+            else:
+                current = 0 
+
+        return total
